f6f52498e3c7889b579c569c9bacad70990e0fd8
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2022-12-08 17:08:40 +0530

    update

--- 6 Dec 2022/3. compound_interest.c
+++ 6 Dec 2022/3. compound_interest.c
@@ -6,12 +6,12 @@ int main(){
 	printf("Enter principal : ");
 	scanf("%f", &principal);
 	printf("Enter rate : ");
-        scanf("%f", &rate);
-	printf("Enter time : ");
-        scanf("%f", &time);
-	printf("Enter times of compounding : ");
-        scanf("%f", &number_Times);
-	compound_interest =principal*pow((1+rate*0.01/number_Times), number_Times*time);
+    scanf("%f", &rate);
+	printf("Enter time (in years) : ");
+    scanf("%f", &time);
+	printf("Enter no. of times interest applied : ");
+	scanf("%f", &numberTimes);
+	compound_interest =principal*pow((1+rate*0.01/numberTimes), numberTimes*time);
 	printf("Final amount after Compound Interest = %.2f \n", compound_interest);
 	return 0;
 }
