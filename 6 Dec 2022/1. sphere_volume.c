cac334b38c70336b02b16ef771d99a53f53ae1ab
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2022-12-08 16:28:34 +0530

    update

--- 6 Dec 2022/1. sphere_volume.c
+++ 6 Dec 2022/1. sphere_volume.c
@@ -5,7 +5,7 @@ int main(){
 	float volume;
 	printf("Enter radius of the sphere : ");
 	scanf("%d",&radius);
-	volume=(float) (4.0/3)*3.14*radius*radius*radius;
+	volume= (4.0/3)*3.14*radius*radius*radius;
 	printf("The volume of sphere is %.2f \n", volume);
 	return 0;
 }
--- 6 Dec 2022/6. equation.c
+++ 6 Dec 2022/6. equation.c
@@ -1,1 +1,7 @@
-//
+//WAP to find the value of t where t=ax^3+bx^2+cx+d, where a, b, c, d are constants
+#include<stdio.h>
+int main()
+{
+    
+    return 0;
+}
