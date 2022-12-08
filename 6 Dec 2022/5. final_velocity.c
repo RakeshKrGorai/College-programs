e7b4e4d3ccfc7d06dc03423e40090ed50cb9d0fa
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2022-12-08 17:36:50 +0530

    update

--- 6 Dec 2022/5. final_velocity.c
+++ 6 Dec 2022/5. final_velocity.c
@@ -4,13 +4,13 @@
 int main()
 {
     float u, v, a, S;
-    printf("Enter value of Initial Velocity : ");
+    printf("Enter value of u : ");
     scanf("%f", &u);
-    printf("Enter value of Acceleration : ");
+    printf("Enter value of a : ");
     scanf("%f", &a);
-    printf("Enter value of Displacement : ");
+    printf("Enter value of S : ");
     scanf("%f", &S);
     v= pow((pow(u,2)+2.0*a*S), 1.0/2);
-    printf("Final Velocity : %.2f", v);
+    printf("The value of V as per the equation is : %.2f", v);
     return 0;
 }
