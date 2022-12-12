5862e4f01be74be4cd439cff4b5ed4b14235d124
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2022-12-12 22:30:01 +0530

    update

--- 9 Dec 2022/11. leap_year.c
+++ 9 Dec 2022/11. leap_year.c
@@ -1,21 +1,18 @@
-//WAP to check whether a given year is leap year or not using nested if...else
+//WAP to check whether leap year or not.
 #include<stdio.h>
 int main()
 {
     int year;
     printf("Enter any year to check : ");
     scanf("%d", &year);
-    if(year%400==0){
-        printf("%d is a leap year", year);
+    if((year%400==0)||(year%4==0)){
+        printf("%d is a leap year \n", year);
     }
     else if(year%100==0){
-        printf("%d is not a leap year", year);
-    }
-    else if(year%4==0){
-        printf("%d is a leap year", year);
+        printf("%d is not a leap year \n", year);
     }
     else{
-        printf("%d is not a leap year", year);
+        printf("%d is not a leap year \n", year);
     }
     return 0;
 }
