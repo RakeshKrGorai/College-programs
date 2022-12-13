aada01d8db19bcce14518da31afc777675dd093e
Rakesh Kumar Gorai <rakesh.k.gorai@gmail.com>
2022-12-13 10:21:49 +0530

    9 Dec 2022

---
+++ 9 Dec 2022/pallindrome.c
@@ -0,0 +1,18 @@
+//WAP to check 3 digit pallindrome no without using loop
+#include<stdio.h>
+int main(){
+	int number;
+	printf("Enter any three number to check : ");
+	scanf("%d", &number);
+	int firstDigit = number/100;
+	int lastDigit = number%10;
+	if(firstDigit == lastDigit){
+		printf("%d is a palllindrome number \n", number);
+	}
+	else{
+		printf("%d is not a pallindrome number \n", number);
+	}
+	return 0;
+
+}
+
