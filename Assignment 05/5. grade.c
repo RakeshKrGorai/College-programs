2f383b104f3aa59e795f4c6fde39e056bb294030
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2022-12-14 23:48:15 +0530

    13 dec 2022

--- Assignment 05/5. grade.c
+++ Assignment 05/5. grade.c
@@ -2,6 +2,32 @@
 #include<stdio.h>
 int main()
 {
-    
+    int hardness, strength, grade;
+    float content;
+    printf("Enter hardness of steel : ");
+    scanf("%d", &hardness);
+    printf("Enter carbon content of steel : ");
+    scanf("%f", &content);
+    printf("Enter tensile strength of steel : ");
+    scanf("%d", &strength);
+    if (hardness>50 && content<0.7 && strength>5600){
+        grade=10;
+    }
+    else if(hardness>50 && content<0.7 && strength<5600){
+        grade=9;
+    }
+    else if(hardness<50 && content<0.7 && strength>5600){
+        grade=8;
+    }
+    else if(hardness>50 && content>0.7 && strength>5600){
+        grade=7;
+    }
+    else if(hardness>50 || content<0.7 || strength>5600){
+        grade=6;
+    }
+    else{
+        grade=5;
+    }
+    printf("Steel with given details has a %d grade", grade);
     return 0;
 }
