5ae1ba0252e607409786fa9dcb6eecf26d1fd4b7
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2022-12-15 00:58:05 +0530

    update

---
+++ Assignment 05/Switch Case/2. operations.c
@@ -0,0 +1,28 @@
+//Enter 2 operands and an operator then do the calculation
+#include<stdio.h>
+int main()
+{
+    int operand1, operand2;
+    char operator;
+    printf("Enter two numbers : ");
+    scanf("%d %d", &operand1, &operand2);
+    printf("Enter operator to be used for the provided operands\n+ for addition \n- for subraction \n* for multiplication \n/ for division \nEnter choice : ");
+    scanf("%s", &operator);
+    switch (operator)
+    {
+    case '+':
+        printf("Sum is : %d", operand1+operand2);
+        break;
+    case '-':
+        printf("Difference is %d", operand1-operand2);
+        break;
+    case '*':
+        printf("Product is %d", operand1*operand2);
+        break;
+    case '/':
+        printf("Quotient is %d", operand1/operand2);
+    default:
+        break;
+    }
+    return 0;
+}
