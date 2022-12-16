c7c3c54ad75adc35abc0827422c365004f73a0ab
Rakesh Kumar Gorai <rakesh.k.gorai@gmail.com>
2022-12-16 16:51:55 +0530

    Update 11. leap_year.c

--- Assignment 04/11. leap_year.c
+++ Assignment 04/11. leap_year.c
@@ -5,11 +5,14 @@ int main()
     int year;
     printf("Enter any year to check : ");
     scanf("%d", &year);
-    if((year%400==0)||(year%4==0)){
+    if(year%400==0){
         printf("%d is a leap year \n", year);
     }
     else if(year%100==0){
         printf("%d is not a leap year \n", year);
+    }
+    else if (year%4==0){
+            printf("%d is a leap year\n", year);
     }
     else{
         printf("%d is not a leap year \n", year);
