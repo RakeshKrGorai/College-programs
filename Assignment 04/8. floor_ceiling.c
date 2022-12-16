0f0bdf5ef89e3b3827a938188043542e0e880f56
Rakesh Kumar Gorai <rakesh.k.gorai@gmail.com>
2022-12-16 15:30:16 +0530

    Update 8. floor_ceiling.c

--- Assignment 04/8. floor_ceiling.c
+++ Assignment 04/8. floor_ceiling.c
@@ -1,8 +1,18 @@
 //WAP to round a given floating point number to integer by considering the floor & ceiling operation, without using built in function
 #include<stdio.h>
 int main(){
-    int number;
-    printf("Enter a number : ");
-    scanf("%d", &number);
-    int fval= (number-1);
+        float x, ceil, floor;
+        printf("Enter a number : ");
+        scanf("%f", &x);
+        if (x-(int)x>0){
+                ceil= (int)x+1;
+                floor = (int)x;
+        }
+        else{
+                ceil = (int)x;
+                floor = (int)x;
+        }
+        printf("floor : %.2f\n", floor);
+        printf("ceil : %.2f\n", ceil);
+        return 0;
 }
