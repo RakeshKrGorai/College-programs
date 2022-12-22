1cbb3f9041508c70dd20e090c2aa1dce1b72a9b6
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2022-12-22 17:39:08 +0530

    20 dec 2022

--- Assignment 06/fibonacci.c
+++ Assignment 06/fibonacci.c
@@ -1,7 +1,17 @@
 //WAP to print n fibonacci numbers
 #include<stdio.h>
 int main(){
-	int num, a=1, b=0;
+	int num, a=0, b=1, c=0, counter=0;
 	printf("Enter value of n : ");
 	scanf("%d", &num);
-
+	printf("%d", a);
+	while (counter<=num)
+	{
+		c=a;
+		a+=b;
+		b=c;
+		printf(" %d", a);
+		counter++;
+	}
+	
+}
