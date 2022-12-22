e697e95c864ca3f5392ec86d28122f3ec7e26a6a
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2022-12-22 17:52:11 +0530

    20 dec 2022

--- Assignment 06/range_prime.c
+++ Assignment 06/range_prime.c
@@ -1,1 +1,20 @@
-//WAP t
+//WAP to print prime numbers within given range
+#include<stdio.h>
+int main()
+{
+    int initial, final, counter=0;
+    printf("Enter starting and end range : ");
+    scanf("%d %d", &initial, &final);
+    for(initial; initial<=final; initial++){
+        for(int i=2;i<initial;i++){
+            if(initial%i==0){
+                counter++;
+            }
+        }
+        if(counter==0){
+            printf("%d ", initial);
+        }
+        counter=0;
+    }
+    return 0;
+}
