8ae6dbbb48121f713577c2c635baf2f7ea714747
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2022-12-22 18:06:22 +0530

    update

--- Assignment 06/2. sum_square.c
+++ Assignment 06/2. sum_square.c
@@ -4,7 +4,12 @@ int main(){
 	int num, sum;
 	printf("Enter value of n : ");
 	scanf("%d", &num);
-	sum= num*(num+1)*(2*num+1)/6;
+	/*Using formula(not what teacher wants)
+	sum= num*(num+1)*(2*num+1)/6;*/
+	// Program teacher is expecting(loop)
+	for(int i=1;i<=num;i++){
+		sum+=i*i;
+	}
 	printf("The sum of squares of first %d natural numbers is %d \n", num, sum);
 	return 0;
 }
