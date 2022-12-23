f14ffd8c8c2fd55da25fa94d19d29dc585f5b7fe
Rakesh Kumar Gorai <rakesh.k.gorai@gmail.com>
2022-12-23 15:22:30 +0530

    update

--- Assignment 06/13. multiplication_table.c
+++ Assignment 06/13. multiplication_table.c
@@ -6,7 +6,7 @@ int main()
     printf("Enter any number : ");
     scanf("%d", &num);
     for(int i=1;i<=10;i++){
-        printf("%d X %d = %d\n", num, i, num*i);
+        printf("%d\t X %d\t = %d\n", num, i, num*i);
     }
     return 0;
 }
