9f9dbe2b70a2fd9387392ad34281aedd1936589d
Rakesh Kumar Gorai <rakesh.k.gorai@gmail.com>
2022-12-27 11:23:28 +0530

    27 dec 2022

---
+++ Assignment 07/pattern2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+   int i, space, k = 0;
+   for (i = 1; i <= 5; ++i, k = 0) {
+      for (space = 1; space <= 5 - i; ++space) {
+         printf("  ");
+      }
+      while (k != 2 * i - 1) {
+         printf("* ");
+         ++k;
+      }
+      printf("\n");
+   }
+   return 0;
+}
+
