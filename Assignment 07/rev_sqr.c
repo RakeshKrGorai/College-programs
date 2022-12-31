af137e6f74baea5889e7d29d65761b066de30b28
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2022-12-31 15:59:58 +0530

    assignment 7

--- Assignment 07/rev_sqr.c
+++ Assignment 07/rev_sqr.c
@@ -1,6 +1,28 @@
 #include<stdio.h>
 int main()
 {
+    int num=1, rev=0, rev_sq, sq, cp1, cp2, digit, cnt=0;
+    for(num;num<=100;num++){
+        sq=num*num;
+        cp1=num;
+        while (cp1){
+            digit=cp1%10;
+            cp1/=10;
+            rev=rev*10+digit;
+        }
+        rev_sq=rev*rev;
+        cp2=rev_sq;
+        while(cp2){
+            digit=cp2%10;
+            cp2/=10;
+            cnt=cnt*10+digit;
+        }
+        if(cnt==sq){
+            printf("%d \t", num);
+        }
+        rev=0;
+        cnt=0;
+    }
     
     return 0;
 }
