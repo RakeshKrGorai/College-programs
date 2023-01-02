b53d4ac014c4c9966c43e6a14e8fed8aa8c2f5bd
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2023-01-03 03:08:03 +0530

    up

--- Assignment 06/12. perfect_even.c
+++ Assignment 06/12. perfect_even.c
@@ -1,7 +1,6 @@
 //WAP to find out perfect squares which are even numbers within 1-10000
 #include<stdio.h>
 int main(){
-	float root;
 	for(int i=2;i<=10000;i+=2){
 	    int sq=i*i;
 		if((sq)<=10000){
