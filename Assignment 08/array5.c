2f5fb48d45621234951862d51e26bfe466068cb9
Rakesh Kumar Gorai <rakesh.k.gorai@gmail.com>
2023-01-06 15:36:39 +0530

    Update array5.c

--- Assignment 08/array5.c
+++ Assignment 08/array5.c
@@ -5,12 +5,19 @@ int main(){
 	int n= sizeof(a)/sizeof(a[0]);
 	printf("Enter position to delete : ");
 	scanf("%d", &pos);
-	for(i=pos;i<n-1;i++){
-		a[i]=a[i+1];
+	if(n<pos){
+	    printf("Err... position  value is greater than length of array");
 	}
-	n--;
-	for(i=0;i<n;i++){
-		printf("%d ", a[i]);
+	else{
+	    printf("Element at %d position is deleted. \n", pos);
+	    printf("Updated Array : \n");
+	    for(i=pos;i<n-1;i++){
+		    a[i]=a[i+1];
+	    }
+	    n--;
+	    for(i=0;i<n;i++){
+		    printf("%d ", a[i]);
+	    }
 	}
 	return 0;
 }
