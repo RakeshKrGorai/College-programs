cc28bb5bd6a21295105417c4c5017c4c6cdd9306
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2023-01-17 19:39:40 +0530

    update

---
+++ Assignment 08/array5.c
@@ -0,0 +1,27 @@
+//Incomplete
+//Find Odd, Eve, +ve and -ve numbers in an Array
+#include<stdio.h>
+int main()
+{
+    int arr[]={52,78,56,-79,12,-35,-64,20}, i=0, len;
+    len=sizeof(arr)/sizeof(arr[0]);
+    for(i;i<len;i++){
+        if (arr[i]>0){
+            if(arr[i]%2==0){
+                printf("Even & Positive Number : %d", arr[i]);
+            }
+            else{
+                printf("Odd & Positive Number : %d", arr[i]);
+            }
+        }
+        else{
+            if(arr[i]%2==0){
+                printf("Even & Positive Number : %d", arr[i]);
+            }
+            else{
+                printf("Odd & Positive Number : %d", arr[i]);
+            }
+        }
+    }
+    return 0;
+}
---
+++ Assignment 08/array7.c
@@ -0,0 +1,17 @@
+//Insert element at a given index
+#include<stdio.h>
+int main()
+{
+    int arr[]={10,22,45,68,98};
+    int index, num, i, n=10;
+    printf("Enter number to be inserted : ");
+    scanf("%d", &num);
+    printf("Enter index (0-9) : ");
+    scanf("%d", &num);
+    for(i=0;i<n;i++){
+        if(i==index){
+            
+        }
+    }
+    return 0;
+}
