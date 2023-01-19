5484c0df68f8ebd737d0c370dc3328b29a312339
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2023-01-19 19:40:40 +0530

    Update

--- Assignment 08/array1.c
+++ Assignment 08/array1.c
@@ -1,12 +1,17 @@
 #include<stdio.h>
 int main(){
-	int arr[20]={0};
+	int arr[20]={10,20,30,40,50,60,70,80,100,90}, i;
 	int pos,val;
 	printf("Enter value to be inserted : ");
 	scanf("%d", &val);
-	printf("Enter position (0-20) : ");
+	printf("Enter position (0-10) : ");
 	scanf("%d", &pos);
 	arr[pos]=val;
 	printf("%d has been placed at %d position \n", val, pos);
+	printf("Updated Array :");
+	for(i=0;i<10;i++){
+		printf("%d \n", arr[i]);
+	}
 	return 0;
+
 }
