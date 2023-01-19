fb32be9436d2d4667f7e9be701c3290af76a7df6
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2023-01-20 00:56:22 +0530

    Update

--- Assignment 07/perfect_ranged.c
+++ Assignment 07/perfect_ranged.c
@@ -1,11 +1,11 @@
 //WAP to print perfect numbers upto given range
 #include<stdio.h>
 int main(){
-    int sum=0, fRange;
+    int sum=0, fRange, i, j;
     printf("Enter final range : ");
     scanf("%d", &fRange);
-    for(int i=1;i<=fRange;i++){
-        for(int j=1;j<i;j++){
+    for(i=1;i<=fRange;i++){
+        for(j=1;j<i;j++){
             if(i%j==0){
                 sum+=j;
             }
