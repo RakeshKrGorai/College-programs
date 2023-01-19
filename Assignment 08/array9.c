7e4f9657f0c2fb047756c9fd366f4d78d47ee948
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2023-01-20 04:03:29 +0530

    Update

--- Assignment 08/array9.c
+++ Assignment 08/array9.c
@@ -1,14 +1,20 @@
-//Reverse array without using another array
 #include<stdio.h>
 int main()
 {
-    int arr[]={1,2,3,4,5,6,7,8,9,10}, i, temp, n=10;
-    for(i=0;i<n/2;i++){
-        temp=arr[i];
-        arr[i]=arr[n-1-i];
-        arr[n-1-i]=temp;
-    }
-    for(i=0;i<n;i++){
+    int arr[]={52,78,56,-79,12,-35,-64,20}, i, j, temp;
+    for (i = 0; i < 8; ++i) 
+        {
+            for (j = i + 1; j < 8; ++j) 
+            {
+                if (arr[i] < arr[j]) 
+                {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    for(i=0;i<8;i++){
         printf("%d ", arr[i]);
     }
     return 0;
