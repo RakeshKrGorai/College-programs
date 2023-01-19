d123c0a74c733dfbd13696fffd07562b2c996030
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2023-01-19 22:52:54 +0530

    FINALLY DONE!!!!!

--- Assignment 07/pattern6.c
+++ Assignment 07/pattern6.c
@@ -1,6 +1,32 @@
 #include<stdio.h>
 int main()
 {
-    
+    int i, j, counter=1;
+    for(i=0;i<5;i++){
+        if(i%2==0){
+            for(j=0;j<10-i;j++){
+                if(j>=i && j%2==0){
+                    printf("%d ", counter);
+                }
+                else{
+                    printf(" ");
+                }
+            }
+            counter++;
+        }
+        else{
+            for(j=0;j<10-i;j++){
+                if(j>=i && j%2!=0){
+                    printf("%d ", counter);
+                }
+                else{
+                    printf(" ");
+                }
+            }
+            counter++;
+        }
+        
+        printf("\n");
+    }
     return 0;
 }
