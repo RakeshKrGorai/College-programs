c86aad807095796c0e3f1d883ad3c2343a5c5e53
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2023-01-20 04:07:05 +0530

    Update

--- Assignment 08/array9.c
+++ Assignment 08/array9.c
@@ -2,11 +2,9 @@
 int main()
 {
     int arr[]={52,78,56,-79,12,-35,-64,20}, i, j, temp;
-    for (i = 0; i < 8; ++i) 
-        {
-            for (j = i + 1; j < 8; ++j) 
-            {
-                if (arr[i] < arr[j]) 
+    for (i=0;i<8;++i){
+            for (j=i+1;j<8;++j) {
+                if (arr[i]<arr[j]) 
                 {
                     temp = arr[i];
                     arr[i] = arr[j];
