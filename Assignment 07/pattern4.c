2fb554583590c9ee768cae99e3ce601517474a99
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2023-01-19 22:02:23 +0530

    Update

--- Assignment 07/pattern4.c
+++ Assignment 07/pattern4.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main()
 {
+	int i, j;
     char ch='A';
-	for(int i=1;i<=5;i++){
-		for(int j=1;j<=i;j++){
+	for(i=1;i<=5;i++){
+		for(j=1;j<=i;j++){
 			printf("%c ",ch);
 		}
 		printf("\n");
