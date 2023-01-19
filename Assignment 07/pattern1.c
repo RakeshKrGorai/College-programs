2b3c15b9eb33e8d77ae0530bc43d2d29839a1d57
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2023-01-19 21:53:44 +0530

    Update

--- Assignment 07/pattern1.c
+++ Assignment 07/pattern1.c
@@ -1,9 +1,10 @@
 //WAP to print following pattern
 #include<stdio.h>
 int main(){
+	int i, j;
 	printf("* \n");
-	for(int i=1;i<9;i+=2){
-		for (int j=0;j<i+2;j++){
+	for(i=1;i<9;i+=2){
+		for (j=0;j<i+2;j++){
 			printf("* ");
 		}
 		printf("\n");
