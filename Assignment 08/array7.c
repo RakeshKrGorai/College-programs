f6a07bd3e45b5200a8a4704db8d3d73d4ee4b4cb
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2023-01-19 16:13:01 +0530

    Update

--- Assignment 08/array7.c
+++ Assignment 08/array7.c
@@ -1,17 +1,20 @@
-//Insert element at a given index
+//Insert element at a given index in a array
 #include<stdio.h>
 int main()
 {
-    int arr[]={10,22,45,68,98};
+    int arr[100]={10,22,45,68,98,78,64,24,87,21};
     int index, num, i, n=10;
     printf("Enter number to be inserted : ");
     scanf("%d", &num);
     printf("Enter index (0-9) : ");
-    scanf("%d", &num);
+    scanf("%d", &index);
+    n++;
+    for(i=9;i>=index;i--){
+        arr[i+1]=arr[i];
+    }
+    arr[index]=num;
     for(i=0;i<n;i++){
-        if(i==index){
-            
-        }
+        printf("%d ", arr[i]);
     }
     return 0;
 }
