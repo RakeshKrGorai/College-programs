1410738b03a2250d0ee9ba70323d1f8da8214468
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2023-01-19 15:08:14 +0530

    update

--- Assignment 09/transpose_matrix.c
+++ Assignment 09/transpose_matrix.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 int main()
 {
-    int arr1[3][3]={{1,2,3},{4,5,6},{7,8,9}}, arr[3][3]={0}, i, j;
+    int arr1[3][3]={{1,2,3},{4,5,6},{7,8,9}}, arr[3][3]={0}, i, j, temp;
     for(i=0;i<3;i++){
 		for(j=0;j<3;j++){
            temp=arr1[i][j];
