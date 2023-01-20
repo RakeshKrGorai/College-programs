f8da8256cbe098d8fa36c717b70aa944890ee5f4
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2023-01-20 10:11:43 +0530

    Update

--- Assignment 08/array10.c
+++ Assignment 08/array10.c
@@ -1,23 +1,24 @@
 #include<stdio.h>
 int main()
 {
-    int arr[]={1,2,3,4,5,6,7,8,9,10},l=0,r=9,m, num;
+    int arr[]={1,2,3,4,5,6,7,8,9,10},low=0,high=9,mid, num;
     printf("Enter a number to search : ");
     scanf("%d", &num);
-    m=(l+r)/2;
-    while(l<=r){
-        if(arr[m]==num){
-            printf("Found at %d position", m);
-        }
-        else if(num>arr[m]){
-            l=m+1;
+    while(low<=high){
+        mid=low+(high-low)/2;
+        if(arr[mid]==num){
+            printf("Found at %d position", mid);
+            break;
         }
         else{
-            r=m-1;
+            if(arr[mid]<num){
+                low=mid+1;
+            }
+            else if(num<arr[mid]){
+                high=mid-1;
+            }
         }
+        
     }
-    
-
-
     return 0;
 }
