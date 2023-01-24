3bd16d4a8995b6d1ececefdeee3684d046641d5d
Rakesh Kumar Gorai <rakesh.k.gorai@gmail.com>
2023-01-24 11:34:40 +0530

    24 Jan 2023
    Incomplete program, dont just copy blindly

---
+++ Assignment 10/factorial.c
@@ -0,0 +1,16 @@
+//WAP to print factorial of a number
+#include<stdio.h>
+int factorial(int num, int fact);
+
+int main(){
+	int num, i;
+	printf("Enter a number : ");
+	scanf("%d", &num);
+	factorial(num;
+	return 0;
+}
+int factorial(int num, int fact){
+	int fact;
+	if(num){
+		fact=fact*factorial(n-1);
+		}
