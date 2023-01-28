0c4aecc8b4685daf39bace5ddcd0607ed4ddd3e3
Rakesh <rakesh.k.gorai@gmail.com>
2023-01-28 11:35:29 +0530

    Update

--- Assignment 10/6. factorial.c
+++ Assignment 10/6. factorial.c
@@ -2,19 +2,23 @@
 
 #include<stdio.h>
 int factorial(int num);
-
-int main(){
-	int num, i;
-	printf("Enter a number : ");
-	scanf("%d", &num);
-	printf("The factorial of %d is %d",num, factorial(num));
-	return 0;
+int main()
+{
+    int num;
+    printf("Enter a number : ");
+    scanf("%d", &num);
+    printf("The factorial of %d is %d", num, factorial(num));
+    return 0;
 }
 int factorial(int num){
-	if(num!=1){
-		return num*factorial(num-1);
-	}
-	else{
-	    return 1;
-	}
+    if(num!=1){
+        int i=1, fact;
+        for(i;i<num;i++){
+            fact*=i;
+        }
+        return fact;
+    }
+    else{
+        return 1;
+    }
 }
