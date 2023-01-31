73de27a1004e751a219205d2b15dfc8f09348882
Rakesh Kumar Gorai <rakesh.k.gorai@gmail.com>
2023-01-31 10:21:58 +0530

    31 Jan 2023

---
+++ Assignment 10/factorial_rec.c
@@ -0,0 +1,20 @@
+//WAP to print factorial of a number
+
+#include<stdio.h>
+int factorial(int num);
+
+int main(){
+	int num, i;
+	printf("Enter a number : ");
+	scanf("%d", &num);
+	printf("The factorial of %d is %d \n",num, factorial(num));
+	return 0;
+}
+int factorial(int num){
+	if(num!=1){
+		return num*factorial(num-1);
+	}
+	else{
+	    return 1;
+	}
+}
