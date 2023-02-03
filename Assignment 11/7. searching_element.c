a23e2168cb730b4b394ab4df6c672aebf725c0e8
Rakesh <rakesh.k.gorai@gmail.com>
2023-02-03 14:49:26 +0530

    31 Jan 2023

---
+++ Assignment 11/7. searching_element.c
@@ -0,0 +1,27 @@
+//WAP to create a function to search value in an array
+#include<stdio.h>
+int searchElement(int arr[], int element);
+int main()
+{
+    int arr[]={1,2,3,4,5,6,7,8,9,0}, element;
+    printf("Enter element to be searched : ");
+    scanf("%d", &element);
+    searchElement(arr, element);
+    return 0;
+}
+int searchElement(int arr[], int element){
+    int i, cnt;
+    for(i=0;i<10;i++){
+        if(arr[i]==element){
+            printf("Element found at %d position", i);
+            cnt++;
+            break;
+        }
+        else{
+            continue;
+        }
+    }
+    if(cnt==0){
+        printf("Element not found");
+    }
+}
