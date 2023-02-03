8090d19ae62e0497d5007be27bddba23b854d83a
Rakesh <rakesh.k.gorai@gmail.com>
2023-02-03 15:01:16 +0530

    31 Jan 2023

---
+++ Assignment 11/8. distance.c
@@ -0,0 +1,17 @@
+//WAP to find distance between 2 points where distance is measured in terms of x and y coordinates. Values are given as 2 array one for x and another for y coordinates.
+
+//Incomplete
+
+#include<stdio.h>
+int distance(int x[], int y[]);
+int main()
+{
+    int x[]={2,4};
+    int y[]={6,3};
+    distance(x,y);
+    return 0;
+}
+int distance(int x[], int y[]){
+    int x_diff, y_diff;
+
+}
