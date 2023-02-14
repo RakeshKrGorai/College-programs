b2a4067bb825ad139edd317e03ffe002bf3b77dd
Rakesh Kumar Gorai <rakesh.k.gorai@gmail.com>
2023-02-14 11:32:42 +0530

    14 Feb 2023

---
+++ Assignment 13/print_reverse.c
@@ -0,0 +1,8 @@
+//WAP to print string in reverse order using user defined function having pointer as argument
+#include<stdio.h>
+int reversePrint();
+int main(){
+	return 0;
+}
+int reversePrint(){
+}
