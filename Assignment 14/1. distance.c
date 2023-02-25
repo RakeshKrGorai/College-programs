752ca12238869784118d546c07a34e60cbaf4c14
Rakesh <rakesh.k.gorai@gmail.com>
2023-02-25 22:56:14 +0530

    25 feb 2023

---
+++ Assignment 14/1. distance.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<math.h>
+typedef struct distance {
+    float x, y;
+} distance;
+
+int main()
+{
+    distance start, end;
+    float x_diff, y_diff;
+    printf("Enter x-y coordinates for point 1 : ");
+    scanf("%f %f", &start.x, &start.y);
+    printf("Enter x-y coordinates for point 2 : ");
+    scanf("%f %f", &end.x, &end.y);
+    float dist;
+    x_diff=end.x-start.x;
+    y_diff=end.y-end.x;
+    x_diff=pow(x_diff, 2);
+    y_diff=pow(y_diff, 2);
+    int sum=x_diff+y_diff;
+    dist=sqrt(sum);
+    printf("Distance : %.2f \n", dist);
+    return 0;
+}
