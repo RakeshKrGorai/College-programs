af82bddce7d788733d8e84443fb8a8d8e9ca9270
Rakesh <rakesh.k.gorai@gmail.com>
2023-02-26 12:15:34 +0530

    Update

--- Assignment 14/1. distance.c
+++ Assignment 14/1. distance.c
@@ -7,14 +7,14 @@ typedef struct distance {
 int main()
 {
     distance start, end;
-    float x_diff, y_diff;
+    int x_diff, y_diff;
     printf("Enter x-y coordinates for point 1 : ");
     scanf("%f %f", &start.x, &start.y);
     printf("Enter x-y coordinates for point 2 : ");
     scanf("%f %f", &end.x, &end.y);
     float dist;
     x_diff=end.x-start.x;
-    y_diff=end.y-end.x;
+    y_diff=end.y-start.y;
     x_diff=pow(x_diff, 2);
     y_diff=pow(y_diff, 2);
     int sum=x_diff+y_diff;
