45aac1ad963ee6b05a46ec89aa1b22ac2adbd49d
Rakesh <rakesh.k.gorai@gmail.com>
2023-02-27 13:26:49 +0530

    Update

--- Assignment 06/8. fibonacci.c
+++ Assignment 06/8. fibonacci.c
@@ -4,13 +4,13 @@ int main(){
 	int num, a=0, b=1, c=0, counter=0;
 	printf("Enter value of n : ");
 	scanf("%d", &num);
-	printf("%d", a);
-	while (counter<=num)
+	//printf("%d", a);
+	while (counter<num)
 	{
+		printf(" %d", a);
 		c=a;
 		a+=b;
 		b=c;
-		printf(" %d", a);
 		counter++;
 	}
 	return 0;
