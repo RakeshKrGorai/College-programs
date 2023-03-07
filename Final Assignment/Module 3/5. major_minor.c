26719d29b9d52efe0b9f975118d7b4577118e2a2
Rakesh <rakesh.k.gorai@gmail.com>
2023-03-07 23:49:57 +0530

    07/03

---
+++ Final Assignment/Module 3/5. major_minor.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int row, column, i, j, min, max;
+    printf("Enter number of rows and columns : ");
+    scanf("%d %d", &row, &column);
+    if(row!=column){
+        printf("Rows and columns should be same, retry\n");
+    }
+    int arr[row][column];
+    for(i=0;i<row;i++){
+        for(j=0;j<column;j++){
+            printf("Enter element :");
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    max=arr[0][0];
+    min=max;
+    for(i=0;i<row;i++){
+        for(j=0;j<column;j++){
+            if(i==j){
+                if(max<arr[i][j]){
+                    max=arr[i][j];
+                }
+                if(min>arr[i][j]){
+                    min=arr[i][j];
+                }
+            }
+        }
+    }
+    printf("Major Diagonal Element : %d\n", max);
+    printf("Minor Diagonal Element : %d\n", min);
+    return 0;
+}
