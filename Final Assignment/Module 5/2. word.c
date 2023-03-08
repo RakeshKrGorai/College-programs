0ed664bfc525622b7abb1f65ee7eab0d35b8d6af
Rakesh <rakesh.k.gorai@gmail.com>
2023-03-08 12:53:47 +0530

    08/03

---
+++ Final Assignment/Module 5/2. word.c
@@ -0,0 +1,18 @@
+#include <stdio.h>  
+#include <stdlib.h>  
+   
+int main()  
+{   char ch;
+    FILE *file;
+    int count = 0;
+    //Opens a file in read mode  
+    file = fopen("data.txt","r"); 
+    while((ch=fgetc(file))!=EOF){
+        if(ch ==' ' || ch == '\n'){
+            count++;
+        }
+    } 
+    printf("Number of words present in given file: %d", count);
+    fclose(file);
+    return 0;
+}  
