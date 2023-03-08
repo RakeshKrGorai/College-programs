19d8b9284bed5c2bba047ed8f2d2f0e2f74fb758
Rakesh <rakesh.k.gorai@gmail.com>
2023-03-08 16:20:29 +0530

    08/03

--- Final Assignment/Module 5/5. string_copy.c
+++ Final Assignment/Module 5/5. string_copy.c
@@ -1,9 +1,33 @@
 #include<stdio.h>
-void Xstrcpy();
+#include<stdlib.h>
+
+void Xstrcpy(char *string);
 int main()
 {
-    int *ptr;
-    char string[]="Rakesh";
-    
-    return 0;
+    char *string = malloc(50*sizeof(char));
+    if(string==NULL){
+        printf("Memory not allocated!");
+    }
+    else{
+        printf("Enter a string : ");
+        scanf("%s", string);
+        Xstrcpy(string);
+        free(string);
+        return 0;
+    }
+}
+void Xstrcpy(char *string){
+    int i;
+    char *copy_string=malloc(50*sizeof(char));
+    if(copy_string==NULL){
+        printf("Memory not allocated!");
+    }
+    else{
+        for(i=0;string[i]!='\0';i++){
+            *(copy_string+i)= *(string+i);
+        }
+        copy_string[i]='\0';
+        printf("Copied String : %s", copy_string);
+        free(copy_string);
+    }
 }
