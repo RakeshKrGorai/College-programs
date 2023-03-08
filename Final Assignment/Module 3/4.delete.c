34712232b999a74886abb7483b610e981485ddc8
Rakesh <rakesh.k.gorai@gmail.com>
2023-03-08 22:34:27 +0530

    Update

--- Final Assignment/Module 3/4.delete.c
+++ Final Assignment/Module 3/4.delete.c
@@ -1,25 +1,24 @@
 #include<stdio.h>
 int main(){
 	int pos, i, j;
-	int a[]={1,2,3,4,5};
+	int a[]={1,2,4,2,5};
 	int n= sizeof(a)/sizeof(a[0]);
 	printf("Enter element to delete : ");
     scanf("%d", &pos);
-    printf("Element at %d position is deleted. \n", pos);
-    printf("Updated Array : \n");
     for(i=0;i<n-1;i++){
-        for(j=i+1;j<n;j++){
-            if(a[i]==pos){
-                
+        if(a[i]==pos){
+            for(j=i+1;j<n;j++){
+                a[j-1]=a[j];
             }
-            a[i]=a[i+1];
+            n--;
         }
+
         
     }
-    n--;
+    printf("Updated Array : \n");
     for(i=0;i<n;i++){
 	    printf("%d ", a[i]);
     }
-	printf("\n");
+    printf("\n");
 	return 0;
 }
