e4e1cb0555f4507d8033bcf2e17ae4c566b9fff5
Rakesh <rakesh.k.gorai@gmail.com>
2023-03-08 22:45:33 +0530

    Update

--- Final Assignment/Module 3/5. major_minor.c
+++ Final Assignment/Module 3/5. major_minor.c
@@ -7,28 +7,30 @@ int main()
     if(row!=column){
         printf("Rows and columns should be same, retry\n");
     }
-    int arr[row][column];
-    for(i=0;i<row;i++){
-        for(j=0;j<column;j++){
-            printf("Enter element :");
-            scanf("%d", &arr[i][j]);
+    else{
+        int arr[row][column];
+        for(i=0;i<row;i++){
+            for(j=0;j<column;j++){
+                printf("Enter element :");
+                scanf("%d", &arr[i][j]);
+            }
         }
-    }
-    max=arr[0][0];
-    min=max;
-    for(i=0;i<row;i++){
-        for(j=0;j<column;j++){
-            if(i==j){
-                if(max<arr[i][j]){
-                    max=arr[i][j];
-                }
-                if(min>arr[i][j]){
-                    min=arr[i][j];
+        max=arr[0][0];
+        min=max;
+        for(i=0;i<row;i++){
+            for(j=0;j<column;j++){
+                if(i==j){
+                    if(max<arr[i][j]){
+                        max=arr[i][j];
+                    }
+                    if(min>arr[i][j]){
+                        min=arr[i][j];
+                    }
                 }
             }
         }
+        printf("Major Diagonal Element : %d\n", max);
+        printf("Minor Diagonal Element : %d\n", min);
     }
-    printf("Major Diagonal Element : %d\n", max);
-    printf("Minor Diagonal Element : %d\n", min);
     return 0;
 }
