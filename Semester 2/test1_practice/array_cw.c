7d03e8c5e3759cdad82a2045ff09fc4d31b3be57
Rakesh <rakesh.k.gorai@gmail.com>
2023-04-11 18:17:06 +0530

    11-04-23

---
+++ Semester 2/test1_practice/array_cw.c
@@ -0,0 +1,26 @@
+//Rotate array n number of times (Clockwise)
+#include<stdio.h>
+int rotate(int arr[],int len){
+    int i,temp=arr[len-1];
+    for(i=len-1;i>0;i--){
+        arr[i]=arr[i-1];
+    }
+    arr[0]=temp;
+    //Print
+    for(i=0;i<len;i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int arr[]={1,2,3,4,5};
+    int n;
+    printf("Enter number of times to rotate CW : ");
+    scanf("%d",&n);
+    int len;
+    len=sizeof(arr)/sizeof(arr[0]);
+    rotate(arr,len);
+    return 0;
+}
