61cf28d64cf5fe04236f87571526c4925cf30831
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2023-04-16 13:00:33 +0530

    Update

--- Semester 2/10 April 2023/sparse2.c
+++ Semester 2/10 April 2023/sparse2.c
@@ -1,7 +1,5 @@
-//Row Major Matrix to Sparse Matrix
 #include<stdio.h>
-int sparse(int row_major[100][3], int row){
-    int sparse[100][100];
+void createSparse(int row_major[][3], int row,int sparse[][100]){
     int row1,col,i,j,k=1;
     row1=row_major[0][0];
     col=row_major[0][1];
@@ -23,10 +21,10 @@ int sparse(int row_major[100][3], int row){
         }
         printf("\n");
     }
-    
 }
-int readMatrix(int rowmajor[][3], int row){
-	printf("Enter The Sparse Matrix obtained previouly");
+void readMatrix(int row_major[][3], int row){
+	int i,j,col=3;
+	printf("Enter The Sparse Matrix obtained previously \n");
 	for(i=0;i<row;i++){
 		printf("Enter %d elements for row %d : ", col,i
 +1);
@@ -35,11 +33,32 @@ int readMatrix(int rowmajor[][3], int row){
 		}
 	}
 }
+void check(int sparse[][100],int original[][100],int row,int col){
+	int i,j,c=0;
+	for(i=0;i<row;i++){
+		for(j=0;j<col;j++){
+			if(sparse[i][j] != original[i][j]){
+				c++;
+				printf("Different");
+			}
+			else{
+				continue;
+			}
+		}
+	}
+	if(c==0){
+		printf("Same");
+	}
+}
 int main()
 {
-    int i,j,row,col=3,row_major[100][3];
+    int original[100][100]={{0,0,3,0,0},{4,0,0,0,0},{0,9,0,8,0},{0,0,7,0,0},{8,0,0,7,0},{0,0,6,0,4}};
+    int row_major[100][3],row,col,sparse[100][100];
     printf("Enter no of rows obtained in output matrix above :");
     scanf("%d",&row);
     readMatrix(row_major,row);
-    sparse(row_major,row);
+    createSparse(row_major,row,sparse);
+    row=row_major[0][0];
+    col=row_major[0][1]
+    check(sparse,original,row,col);
 }
