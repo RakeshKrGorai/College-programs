d13c24e0595aedaf3d84c9f025d77a356622f2ff
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2023-05-03 16:17:44 +0530

    Final Product

--- infix_postfix.c
+++ infix_postfix.c
@@ -1,79 +1,101 @@
-//Convert infix to postfix using stack.((A-(B+C))*D)^(E/F)
-//What is to be fixed ? Well, i need to use push function just for operators,that is for only stack array, pushing values into postfix array should be 
-//done in main function only
+// Convert infix to postfix using stack.
 
-#include<stdio.h>
-
+#include <stdio.h>
 #define size 100
-int top1=-1;
-char stack[size],postfix[size];
+int top1 = -1;
+char stack[size], postfix[size];
 
-int precedence(char ch){
-	switch(ch){
-		case '+':
-		case '-':
-			return 1;
-		case '*':
-		case '/':
-			return 2;
-		case '^':
-			return 3;
+int precedence(char ch)
+{
+	switch (ch)
+	{
+	case '+':
+	case '-':
+		return 1;
+	case '*':
+	case '/':
+		return 2;
+	case '^':
+		return 3;
+	default:
+		return -1;
 	}
 }
 
-int checker(char ch){
-	while(precedence(ch)<=precedence(stack[top1])){
-		int x=pop();
-	done	push(x,postfix,top2);
-	}
-	push(ch,stack,top1);
+int isAlphabet(char ch)
+{
+	return ((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122));
 }
 
-
-int isAlphabet(char ch){
-	return ((ch>=65 && ch<=90)||(ch>=97 && ch<=122));
+int isDigit(char ch)
+{
+	return (ch >= 48 && ch <= 57);
 }
 
-int isDigit(char ch){
-	return (ch>=48 && ch<=57);
-}
-
-void push(char ch,char arr[],int top){
-	if(top==size-1){
+void push(char ch)
+{
+	if (top1 == size - 1)
+	{
 		printf("Overflow");
 	}
-	else{
-		arr[++top]=ch;
+	else
+	{
+		stack[++top1] = ch;
 	}
 }
 
-char pop(){
-	if(top1==-1){
-		printf("Stack Underflow");
+char pop()
+{
+	if (top1 == -1)
+	{
+		printf("Underflow");
 	}
-	else{
-		return stack[top--];
+	else
+	{
+		return stack[top1--];
 	}
 }
 
-int main(){
+int main()
+{
 	char arr[size];
 	printf("Enter infix expression : ");
-	scanf("%[^\n]",arr);
-	int i,x;
-	for(i=0;arr[i]!='\0';i++){
-		if(isAlphabet || isDigit){
-			push(arr[i],postfix,top2);
+	scanf("%[^\n]", arr);
+	// scanf("%s",arr);
+	int i, top = 0;
+	for (i = 0; arr[i] != '\0'; i++)
+	{
+		if (isAlphabet(arr[i]) || isDigit(arr[i]))
+		{
+			postfix[top++] = arr[i];
 		}
-		else if(arr[i]=='('){
-			push(arr[i],stack,top1);
+		else if (arr[i] == '(')
+		{
+			push(arr[i]);
 		}
-		else if(arr[i]==')'){
-			while(stack[top]!='('){
-				
+		else if (arr[i] == ')')
+		{
+			while (stack[top1] != '(')
+			{
+				postfix[top++] = pop();
+			}
+			pop();
+		}
 
-		else{
-			checker(arr[i]);
+		else
+		{
+			while ((top1 != -1) && (precedence(arr[i]) <= precedence(stack[top1])))
+			{
+				postfix[top++] = pop();
+			}
+			push(arr[i]);
 		}
 	}
+	// Now remaining stack stuff pop and push
+	while (top1 >= 0)
+	{
+		postfix[top++] = pop();
+	}
+	postfix[top] = '\0';
+	printf("%s", postfix);
 }
