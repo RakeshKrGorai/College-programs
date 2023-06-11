7bbf25147e91d79c411c3398276f1a83b0f4fb56
Rakesh <rakesh.k.gorai@gmail.com>
2023-06-11 18:05:58 +0530

    Delete from specific position

--- Semester 2/Assignment 6/delete_linkedList.c
+++ Semester 2/Assignment 6/delete_linkedList.c
@@ -1,161 +1,189 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct node{
+typedef struct node
+{
 	int info;
 	struct node *next;
-}node;
+} node;
 
-node *start=NULL, *temp, *previous, *newnode;
+node *start = NULL, *temp, *previous, *newnode;
 
-void create(){
-	if(start==NULL){
-		start=(node *)malloc(sizeof(node));
+void create()
+{
+	if (start == NULL)
+	{
+		start = (node *)malloc(sizeof(node));
 		printf("Enter an element : ");
 		scanf("%d", &start->info);
 	}
-	else{
-		previous=start;
-		while(previous->next!=NULL){
-			previous=previous->next;
+	else
+	{
+		previous = start;
+		while (previous->next != NULL)
+		{
+			previous = previous->next;
 		}
-		temp= (node *)malloc(sizeof(node));
+		temp = (node *)malloc(sizeof(node));
 		printf("Enter an element");
-		scanf("%d",&temp->info);
-		temp->next=NULL;
-		previous->next=temp;
+		scanf("%d", &temp->info);
+		temp->next = NULL;
+		previous->next = temp;
 	}
 }
 
-void traverse(){
-	if(start==NULL){
+void traverse()
+{
+	if (start == NULL)
+	{
 		printf("List is empty (:|)");
 	}
-	else{
-		temp=start;
-		while(temp!=NULL){
+	else
+	{
+		temp = start;
+		while (temp != NULL)
+		{
 			printf("%d ", temp->info);
-			temp=temp->next;
+			temp = temp->next;
 		}
 		printf("\n");
 	}
 }
 
-void delete_begin(){        
-	if(start==NULL){
+void delete_begin()
+{
+	if (start == NULL)
+	{
 		printf("Empty List");
 	}
-	else{
-		temp=start;
-		start=start->next;
+	else
+	{
+		temp = start;
+		start = start->next;
 		free(temp);
 	}
 }
 
-void delete_end(){
-	if(start==NULL){
+void delete_end()
+{
+	if (start == NULL)
+	{
 		printf("Empty!");
 	}
-	else{
-                temp = start;
-                while((temp->next)->next!=NULL){
-                        temp=temp->next;
-                }
+	else
+	{
+		temp = start;
+		while ((temp->next)->next != NULL)
+		{
+			temp = temp->next;
+		}
 		free((temp->next)->next);
-                temp->next=NULL;
-        }
+		temp->next = NULL;
+	}
 }
 
-
-
-
-void delete_specific(){
-	int position,cnt=1;
-	if(start==NULL){
+void delete_specific()
+{
+	int position, cnt = 1;
+	if (start == NULL)
+	{
 		printf("Nothing to delete here :/");
 	}
-	else{
+	else
+	{
 		printf("Enter position to delete : ");
 		scanf("%d", &position);
-		temp=start;
-		if(position==1){
-			start=start->next;
+		temp = start;
+		if (position == 1)
+		{
+			start = start->next;
 			free(temp);
-
 		}
-		else{
-			while(temp->next!=NULL && cnt!=position){
-				previous=temp;
-				temp=temp->next;
+		else
+		{
+			while (temp->next != NULL && cnt != position)
+			{
+				previous = temp;
+				temp = temp->next;
 				cnt++;
 			}
-			if(temp==NULL && cnt!=position){
+			if (temp == NULL && cnt != position)
+			{
 				printf("Oops, Not enough elements to traverse through...retry");
 			}
-			else{
-				previous->next=temp->next;
+			else
+			{
+				previous->next = temp->next;
 				free(temp);
 			}
 		}
 	}
-
 }
 
-void delete_after(){
+void delete_after()
+{
 	int element;
-	if(start==NULL){
+	if (start == NULL)
+	{
 		printf("Nothing to delete here :)");
 	}
-	else{
+	else
+	{
 		printf("Enter element after which a number is to be deleted : ");
 		scanf("%d", &element);
-		temp=start;
-		while(temp!=NULL && temp->info!=element){
-			previous=temp;
-			temp=temp->next;
+		temp = start;
+		while (temp != NULL && temp->info != element)
+		{
+			previous = temp;
+			temp = temp->next;
 		}
-		if(temp==NULL){
+		if (temp == NULL)
+		{
 			printf("Ayo, that element is not present, retry");
 		}
-		else{
-			previous=temp->next;
-			temp->next=previous->next;
+		else
+		{
+			previous = temp->next;
+			temp->next = previous->next;
 			free(previous);
 		}
 	}
 }
 
-int main(){
+int main()
+{
 	int choice;
-	while(1){
+	while (1)
+	{
 		printf("1. Create LL\n2. Traverse LL\n");
 		printf("3. Delete from beginning\n4. Delete from end\n5. Delete from specific position\n6. Delete after specific node\n7. Exit\n");
 		printf("Enter your choice : ");
 		scanf("%d", &choice);
-		switch(choice){
-			case 1:
-				create();
-				break;
-			case 2:
-				traverse();
-				break;
-			case 3:
-				delete_begin();
-				break;
-			case 4:
-				delete_end();
-				break;
-			case 5:
-				delete_specific();
-				break;
-			case 6:
-				delete_after();
-				break;
-			case 7:
-				return 0;
-			default:
-				printf("Wrong Choice");
-				break;
+		switch (choice)
+		{
+		case 1:
+			create();
+			break;
+		case 2:
+			traverse();
+			break;
+		case 3:
+			delete_begin();
+			break;
+		case 4:
+			delete_end();
+			break;
+		case 5:
+			delete_specific();
+			break;
+		case 6:
+			delete_after();
+			break;
+		case 7:
+			return 0;
+		default:
+			printf("Wrong Choice");
+			break;
 		}
 	}
 }
