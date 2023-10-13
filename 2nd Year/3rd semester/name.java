bbeb3872dac719f134881dbf85a5db177abb3418
Rakesh <rakesh.k.gorai@gmail.com>
2023-10-13 15:06:53 +0530

    13-10-23

---
+++ 2nd Year/3rd semester/name.java
@@ -0,0 +1,11 @@
+// WAP a JAVA program to display your name
+
+import java.lang.System;
+import java.lang.String;
+
+class name { 
+	public static void main(String args[])
+	{
+		System.out.print("Rakesh Kumar Gorai");
+	}
+}
---
+++ 2nd Year/3rd semester/sum.java
@@ -0,0 +1,12 @@
+//WAP to take 2 numbers and add it
+
+import java.lang.System;
+import java.lang.String;
+class sum { 
+	public static void main(String args[])
+	{
+		int num1= 60, num2= 10;
+		int result = num1+num2;
+		System.out.print(result);
+	}
+}
