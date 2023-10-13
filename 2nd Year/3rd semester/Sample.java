0393b0c743d0c3bca4cb6cbf6cf350944ad01a3c
Rakesh <rakesh.k.gorai@gmail.com>
2023-10-13 14:32:25 +0530

    13 Oct 23

---
+++ 2nd Year/3rd semester/Sample.java
@@ -0,0 +1,13 @@
+//importing classes
+import java.lang.System;
+import java.lang.String;
+//import java.lang.* : imports all classes
+
+//Method : main, class : System.out
+
+class Sample { 
+	public static void main(String args[])
+	{
+		System.out.print("Hello World");
+	}
+}
