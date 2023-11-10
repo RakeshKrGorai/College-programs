9f2a62b1d44ca969bb6842fd7210eebc71587374
Rakesh <rakesh.k.gorai@gmail.com>
2023-11-10 13:49:02 +0530

    Question 2

---
+++ 2nd Year/3rd semester/Assignment 2/SignedNumbersZero.java
@@ -0,0 +1,21 @@
+//Write a program to check whether an inputted number is positive or negative.
+
+import java.util.*;
+
+class SignedNumbersZero{
+	public static void main(String[] args){
+		int number;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number to check : ");
+		number = sc.nextInt();
+		if(number>0){
+			System.out.println(number +" is a positive number.");
+		}
+		else if(number == 0){
+			System.out.println(number +" is equal to zero.");
+		}
+		else{
+			System.out.println(number +" is a negative number.");
+		}
+	}
+}
