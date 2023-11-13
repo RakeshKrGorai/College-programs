b859bba7c01a9d902a9da5e38b93e973089bf262
Rakesh <rakesh.k.gorai@gmail.com>
2023-11-13 16:25:22 +0530

    Question 14

---
+++ 2nd Year/3rd semester/Assignment 2/BubbleSort.java
@@ -0,0 +1,46 @@
+//Write a program to sort an array of n integers using bubble sort
+
+import java.util.*;
+
+class MeanArray{
+	public static void main(String[] args){
+		int n, sum=0, temp;
+		double avg;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter value of n : ");
+		n = sc.nextInt();
+		int arr[] = new int[n];
+
+		System.out.print("Enter "+n+" values : ");
+		for(int i=0;i<n;i++){
+			arr[i] = sc.nextInt();
+		}
+
+		System.out.print("Unsorted Array : ");
+		for(int i=0;i<arr.length;i++){
+			System.out.print(arr[i]+ " ");
+		}
+
+		//Bubble Sort Logic
+		
+		for(int i=0;i<arr.length;i++){
+			for(int j=0;j<arr.length-1-i;j++){
+				if(arr[j]>arr[j+1]){
+					temp= arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;;
+				}
+				else{
+					continue;
+				}
+			}
+		}
+
+		System.out.println("Sorted Array : ");
+		for(int i=0;i<arr.length;i++){
+			System.out.print(arr[i]+ " ");
+		}
+
+		//printArray(arr);
+	}
+}
